5d2044092834ffae8d095354cbd88937e13aac77
Borong Lin <1076627773@qq.com>
2022-03-09 21:30:02 +0800

    data base

--- app01/views.py
+++ app01/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def register(request):
     if request.method == 'POST':
         username = request.POST.get("username")
         password = request.POST.get("password")
         email = request.POST.get("email")
-        if len(username)>32 or len(password)>32 or email>64:
+        if len(username) > 32 or len(password) > 32 or email > 64:
             pass
